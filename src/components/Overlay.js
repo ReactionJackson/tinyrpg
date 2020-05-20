b94bf9c8e3adb1e5985f6fbaeabadbc811e9384b
Joe Jackson <hello@josephmjackson.com>
2020-05-21 00:22:11 +0100

    Added a screen transition animation (using O to trigger currently)

---
+++ src/components/Overlay.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { regionSize, blockSize } from '../constants/settings'
+
+const Overlay = () => {
+	
+	const [ isOpen, setIsOpen ] = useState(true)
+	
+	const toggleOverlay = ({ code }) => {
+		if(code === 'KeyO') {
+			setIsOpen(prev => !prev)
+		}
+	}
+	
+	useEffect(() => {
+		document.addEventListener('keyup', toggleOverlay)
+		return () => document.removeEventListener('keyup', toggleOverlay)
+	}, [])
+	
+	return (
+		<div style={{
+			position: 'absolute',
+			right: 0,
+			width: regionSize * blockSize,
+			height: regionSize * blockSize,
+			overflow: 'hidden'
+		}}>
+			<OverlayTop cover={ isOpen } />
+			<OverlayLeft cover={ isOpen } />
+			<OverlayBottom cover={ isOpen } />
+			<OverlayRight cover={ isOpen } />
+		</div>
+	)
+}
+
+const OverlaySide = styled.div`
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background-color: #000;
+	transition: left steps(16) 1s, top steps(16) 1s;
+`
+
+const OverlayTop = styled(OverlaySide)`
+	left: 0;
+	top: ${ ({ cover }) => cover ? '-100%' : 0 };
+`
+
+const OverlayLeft = styled(OverlaySide)`
+	left: ${ ({ cover }) => cover ? '-100%' : 0 };
+	top: 0;
+`
+
+const OverlayBottom = styled(OverlaySide)`
+	left: 0;
+	top: ${ ({ cover }) => cover ? '100%' : 0 };
+`
+
+const OverlayRight = styled(OverlaySide)`
+	left: ${ ({ cover }) => cover ? '100%' : 0 };
+	top: 0;
+`
+
+export default Overlay
